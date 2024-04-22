4594e01ee503f04230152a21899211ec44f296ef
Mahesh Deore <maheshdeore791@gmail.com>
2024-04-22 14:12:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "jenkins-ec2" {
   instance_type = "t2.micro"
   key_name = "org-mdeore-ec2jenkins"
   vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
-  user_data = file("./install_jenkins.sh")
+  user_data = file("install_jenkins.sh")
   tags = {
     Name = "org-devops-jenkinsEC2"
   }
